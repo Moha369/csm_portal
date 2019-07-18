84d4b2b6e6c146d014722c4b248fcb69767271d2
Paul Bennett <pbennett.uk@outlook.com>
2019-07-18 11:44:39 +0100

    login required for home page successful

--- clients/views.py
+++ clients/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .models import Post
 
 
+@login_required
 def home(request):
     context = {
         'posts': Post.objects.all()
