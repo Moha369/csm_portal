b4933ed2ca2b30743a73a0bc6d34b912087c4fba
Paul Bennett <pbennett.uk@outlook.com>
2019-07-18 11:12:03 +0100

    removal of sites in apps

--- csm_portal/settings.py
+++ csm_portal/settings.py
@@ -36,19 +36,12 @@ INSTALLED_APPS = [
     'clients.apps.ClientsConfig',
     'users.apps.UsersConfig',
     'crispy_forms',
-    'social_django',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.sites',
     'django.contrib.staticfiles',
-
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
-    'allauth.socialaccount.providers.google',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +67,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'social_django.context_processors.backends',  # <- Here
-                'social_django.context_processors.login_redirect',  # <- Here
             ],
         },
     },
@@ -113,19 +104,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Google SSO
-# https://fosstack.com/how-to-add-google-authentication-in-django/
-
-AUTHENTICATION_BACKENDS = (
-    # 'social_core.backends.open_id.OpenIdAuth',  # for Google authentication
-    # 'social_core.backends.google.GoogleOpenId',  # for Google authentication
-    # 'social_core.backends.google.GoogleOAuth2',  # for Google authentication
-    # 'social_core.backends.github.GithubOAuth2',  # for Github authentication
-    # 'social_core.backends.facebook.FacebookOAuth2',  # for Facebook authentication
-
-    'django.contrib.auth.backends.ModelBackend',
-)
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -146,8 +124,3 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-
-# LOGIN_URL = 'login'
-# LOGIN_REDIRECT_URL = 'clients-home'
-
-SITE_ID = 1
