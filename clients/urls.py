b5b1fd6c1cffd1fb49dc84e6cb2969cf6bd271f6
Paul Bennett <pbennett.uk@outlook.com>
2019-07-18 17:51:39 +0100

    Part 10 - Create, Delete and Update

--- clients/urls.py
+++ clients/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from django.conf.urls import url, include
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="clients-home"),
+    path('', PostListView.as_view(), name="clients-home"),
+    path('client/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
+    path('client/new/', PostCreateView.as_view(), name="post-create"),
+    path('client/<int:pk>/update/', PostUpdateView.as_view(), name="post-update"),
+    path('client/<int:pk>/delete/', PostDeleteView.as_view(), name="post-delete"),
     path('about/', views.about, name="clients-about"),
     path('links/', views.links, name="clients-links"),
 ]
--- clients/views.py
+++ clients/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 
 
@@ -9,6 +11,66 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'clients/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'clients/home.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['client_ID']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = [
+        'client_name',
+        'client_ID',
+        'client_password',
+        'client_FTP',
+        'client_API_login',
+        'client_API_password',
+    ]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = [
+        'client_name',
+        'client_ID',
+        'client_password',
+        'client_FTP',
+        'client_API_login',
+        'client_API_password',
+    ]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def about(request):
