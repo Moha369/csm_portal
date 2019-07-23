86dd89e48943eb8af6259a45b982f327d76464f6
Paul Bennett <pbennett.uk@outlook.com>
2019-07-23 19:39:25 +0100

    social-auth working as intended

--- csm_portal/urls.py
+++ csm_portal/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
     path('', include('clients.urls')),
+    path('auth/', include('social_django.urls', namespace='social')),  # social auth
 ]
 
 if settings.DEBUG:
